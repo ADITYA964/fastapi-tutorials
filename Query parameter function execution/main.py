5e3e2b5671738094bb0cfb671e960e75281be0fc
ADITYA964 <adityab110699@gmail.com>
2021-12-15 13:09:17 +0530

    Create main.py

---
+++ Query parameter function execution/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+  if published:
+    return {'data': f'{limit} published blogs from the db'}
+  else:
+    return {'data': f'{limit} blogs from the db'}
+    
+class Blog(BaseModel):
+  title: str
+  body: str
+  published: Optional[bool]
+    
+@app.post('/blog')
+def create_blog(blog: Blog):
+  return {'data': f"Blog is created with title as {blog.title}"}
+    
