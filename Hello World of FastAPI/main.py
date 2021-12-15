8811f56cca53c70e7532a3dff736b74473d15011
ADITYA964 <adityab110699@gmail.com>
2021-12-15 14:14:14 +0530

    Update main.py

--- Hello World of FastAPI/main.py
+++ Hello World of FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get('/')
@@ -9,3 +9,7 @@ def index():
 @app.get('/about')
 def about():
   return {'data':'About page.'}
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="127.0.0.1", port=9000)
+  
