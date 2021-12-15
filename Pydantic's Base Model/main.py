02890f0e05eb5c89374aec10ede38a567a13118e
ADITYA964 <adityab110699@gmail.com>
2021-12-15 14:15:17 +0530

    Update main.py

--- Pydantic's Base Model/main.py
+++ Pydantic's Base Model/main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
 
 class Item(BaseModel):
   name: str
@@ -13,3 +14,6 @@ app = FastAPI()
 @app.post("/items/")
 async def create_item(item: Item):
   return item
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="127.0.0.1", port=9000)
